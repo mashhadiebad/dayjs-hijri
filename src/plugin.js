52693879abd72e08c1901297a8452032cbfed68c
M.MashhadiEbad <m.mashhadiebad@parto.com>
2024-09-15 18:23:26 +0330

    fix: Fix package.json

--- src/plugin.js
+++ src/plugin.js
@@ -1,12 +1,12 @@
-import fa from "dayjs/esm/locale/fa";
+import ar from "dayjs/esm/locale/ar";
 
-import jdate from "./calendar";
-import * as C from "./constant";
+import hdate from "./calendar.js";
+import * as C from "./constant.js";
 
 export default (o, Dayjs, dayjs) => {
   const proto = Dayjs.prototype;
   const U = proto.$utils();
-  const $isJalali = (v) => v.$C === "jalali";
+  const $isHijri = (v) => v.$C === "hijri";
   const $prettyUnit = U.prettyUnit || U.p;
   const $isUndefined = U.isUndefined || U.u;
   const $padStart = U.padStart || U.s;
@@ -16,8 +16,8 @@ export default (o, Dayjs, dayjs) => {
     function (...args) {
       const unsure = action.bind(this)(...args);
       unsure.$C = this.$C;
-      if (unsure.isJalali()) {
-        unsure.InitJalali();
+      if (unsure.isHijri()) {
+        unsure.InitHijri();
       }
       return unsure;
     };
@@ -53,21 +53,21 @@ export default (o, Dayjs, dayjs) => {
   proto.calendar = function (calendar) {
     const that = this.clone();
     that.$C = calendar;
-    if (that.isJalali()) {
-      that.InitJalali();
+    if (that.isHijri()) {
+      that.InitHijri();
     }
     return that;
   };
 
-  proto.isJalali = function () {
-    return $isJalali(this);
+  proto.isHijri = function () {
+    return $isHijri(this);
   };
 
-  dayjs.en.jmonths =
-    "Farvardin_Ordibehesht_Khordaad_Tir_Mordaad_Shahrivar_Mehr_Aabaan_Aazar_Dey_Bahman_Esfand".split(
+  dayjs.en.hmonths =
+    "Muharram_Safar_Rabi al-Awwal_Rabi al-Thani_Jumada al-Awwal_Jumada al-Thani_Rajab_Shaban_Ramadan_Shawwal_Dhu al-Qadah_Dhu al-Hijjah".split(
       "_",
     );
-  dayjs.locale("fa", { ...fa, ...C.fa }, true);
+  dayjs.locale("ar", { ...ar, ...C.ar }, true);
 
   const wrapper = function (date, instance) {
     return dayjs(date, {
@@ -80,8 +80,8 @@ export default (o, Dayjs, dayjs) => {
   proto.init = function (cfg = {}) {
     oldInit.bind(this)(cfg);
 
-    if (this.isJalali()) {
-      this.InitJalali();
+    if (this.isHijri()) {
+      this.InitHijri();
     }
   };
 
@@ -96,7 +96,7 @@ export default (o, Dayjs, dayjs) => {
       (reg = cfg.date.match(C.REGEX_PARSE))
     ) {
       // 1397-08-08 or 13970808
-      const [y, m, d] = jdate.G(
+      const [y, m, d] = hdate.G(
         parseInt(reg[1], 10),
         parseInt(reg[2], 10),
         parseInt(reg[3] || 1, 10),
@@ -106,22 +106,22 @@ export default (o, Dayjs, dayjs) => {
     return oldParse.bind(this)(cfg);
   };
 
-  proto.InitJalali = function () {
-    const [jy, jm, jd] = jdate.J(this.$y, this.$M + 1, this.$D);
-    this.$jy = jy;
-    this.$jM = jm - 1;
-    this.$jD = jd;
+  proto.InitHijri = function () {
+    const [hy, hm, hd] = hdate.J(this.$y, this.$M + 1, this.$D);
+    this.$hy = hy;
+    this.$hM = hm - 1;
+    this.$hD = hd;
   };
 
   proto.startOf = function (units, startOf) {
     // startOf -> endOf
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldStartOf.bind(this)(units, startOf);
     }
     const isStartOf = !$isUndefined(startOf) ? startOf : true;
     const unit = $prettyUnit(units);
-    const instanceFactory = (d, m, y = this.$jy) => {
-      const [gy, gm, gd] = jdate.G(y, m + 1, d);
+    const instanceFactory = (d, m, y = this.$hy) => {
+      const [gy, gm, gd] = hdate.G(y, m + 1, d);
       const ins = wrapper(new Date(gy, gm - 1, gd), this);
       return (isStartOf ? ins : ins.endOf(C.D)).$set("hour", 1); // prevent daylight saving issue in safari
     };
@@ -130,31 +130,31 @@ export default (o, Dayjs, dayjs) => {
       case C.Y:
         return isStartOf
           ? instanceFactory(1, 0)
-          : instanceFactory(0, 0, this.$jy + 1);
+          : instanceFactory(0, 0, this.$hy + 1);
       case C.M:
         return isStartOf
-          ? instanceFactory(1, this.$jM)
+          ? instanceFactory(1, this.$hM)
           : instanceFactory(
               0,
-              (this.$jM + 1) % 12,
-              this.$jy + parseInt((this.$jM + 1) / 12, 10),
+              (this.$hM + 1) % 12,
+              this.$hy + parseInt((this.$hM + 1) / 12, 10),
             );
       case C.W:
         return isStartOf
-          ? instanceFactory(this.$jD - WModifier, this.$jM)
-          : instanceFactory(this.$jD + (6 - WModifier), this.$jM);
+          ? instanceFactory(this.$hD - WModifier, this.$hM)
+          : instanceFactory(this.$hD + (6 - WModifier), this.$hM);
       default:
         return oldStartOf.bind(this)(units, startOf);
     }
   };
 
   proto.$set = function (units, int) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return old$Set.bind(this)(units, int);
     }
     const unit = $prettyUnit(units);
-    const instanceFactory = (d, m, y = this.$jy) => {
-      const [gy, gm, gd] = jdate.G(y, m + 1, d);
+    const instanceFactory = (d, m, y = this.$hy) => {
+      const [gy, gm, gd] = hdate.G(y, m + 1, d);
       this.$d.setFullYear(gy);
       this.$d.setMonth(gm - 1);
       this.$d.setDate(gd);
@@ -163,13 +163,13 @@ export default (o, Dayjs, dayjs) => {
     switch (unit) {
       case C.DATE:
       case C.D:
-        instanceFactory(int, this.$jM);
+        instanceFactory(int, this.$hM);
         break;
       case C.M:
-        instanceFactory(this.$jD, int);
+        instanceFactory(this.$hD, int);
         break;
       case C.Y:
-        instanceFactory(this.$jD, this.$jM, int);
+        instanceFactory(this.$hD, this.$hM, int);
         break;
       default:
         return old$Set.bind(this)(units, int);
@@ -179,7 +179,7 @@ export default (o, Dayjs, dayjs) => {
   };
 
   proto.add = function (number, units) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldAdd.bind(this)(number, units);
     }
     number = Number(number); // eslint-disable-line no-param-reassign
@@ -190,19 +190,19 @@ export default (o, Dayjs, dayjs) => {
         : $prettyUnit(units);
     const instanceFactory = (u, n) => {
       const date = this.set(C.DATE, 1).set(u, n + number);
-      return date.set(C.DATE, Math.min(this.$jD, date.daysInMonth()));
+      return date.set(C.DATE, Math.min(this.$hD, date.daysInMonth()));
     };
     if (["M", C.M].indexOf(unit) > -1) {
-      const n = this.$jM + number;
+      const n = this.$hM + number;
       const y = n < 0 ? -Math.ceil(-n / 12) : parseInt(n / 12, 10);
-      const d = this.$jD;
+      const d = this.$hD;
       const x = this.set(C.D, 1)
         .add(y, C.Y)
         .set(C.M, n - y * 12);
       return x.set(C.D, Math.min(x.daysInMonth(), d));
     }
     if (["y", C.Y].indexOf(unit) > -1) {
-      return instanceFactory(C.Y, this.$jy);
+      return instanceFactory(C.Y, this.$hy);
     }
     if (["d", C.D].indexOf(unit) > -1) {
       const date = new Date(this.$d);
@@ -214,31 +214,31 @@ export default (o, Dayjs, dayjs) => {
   };
 
   proto.format = function (formatStr, localeObject) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldFormat.bind(this)(formatStr, localeObject);
     }
     const str = formatStr || C.FORMAT_DEFAULT;
     const locale = localeObject || this.$locale();
-    const { jmonths } = locale;
+    const { hmonths } = locale;
     return str.replace(C.REGEX_FORMAT, (match) => {
       if (match.indexOf("[") > -1) return match.replace(/\[|\]/g, "");
       switch (match) {
         case "YY":
-          return String(this.$jy).slice(-2);
+          return String(this.$hy).slice(-2);
         case "YYYY":
-          return String(this.$jy);
+          return String(this.$hy);
         case "M":
-          return String(this.$jM + 1);
+          return String(this.$hM + 1);
         case "MM":
-          return $padStart(this.$jM + 1, 2, "0");
+          return $padStart(this.$hM + 1, 2, "0");
         case "MMM":
-          return jmonths[this.$jM].slice(0, 3);
+          return hmonths[this.$hM].slice(0, 3);
         case "MMMM":
-          return jmonths[this.$jM];
+          return hmonths[this.$hM];
         case "D":
-          return String(this.$jD);
+          return String(this.$hD);
         case "DD":
-          return $padStart(this.$jD, 2, "0");
+          return $padStart(this.$hD, 2, "0");
         default:
           return oldFormat.bind(this)(match, localeObject);
       }
@@ -246,7 +246,7 @@ export default (o, Dayjs, dayjs) => {
   };
 
   proto.diff = function (input, units, float) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldDiff.bind(this)(input, units, float);
     }
     const unit = $prettyUnit(units);
@@ -270,31 +270,31 @@ export default (o, Dayjs, dayjs) => {
   };
 
   proto.year = function (input) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldYear.bind(this)(input);
     }
-    return this.$g(input, "$jy", C.Y);
+    return this.$g(input, "$hy", C.Y);
   };
 
   proto.month = function (input) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldMonth.bind(this)(input);
     }
-    return this.$g(input, "$jM", C.M);
+    return this.$g(input, "$hM", C.M);
   };
 
   proto.date = function (input) {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldDate.bind(this)(input);
     }
-    return this.$g(input, "$jD", C.D);
+    return this.$g(input, "$hD", C.D);
   };
 
   proto.daysInMonth = function () {
-    if (!$isJalali(this)) {
+    if (!$isHijri(this)) {
       return oldDaysInMonth.bind(this)();
     }
-    return this.endOf(C.M).$jD;
+    return this.endOf(C.M).$hD;
   };
 
   /**
@@ -303,13 +303,13 @@ export default (o, Dayjs, dayjs) => {
    */
   if (oldToArray) {
     proto.toArray = function () {
-      if (!$isJalali(this)) {
+      if (!$isHijri(this)) {
         return oldToArray.bind(this)();
       }
       return [
-        this.$jy,
-        this.$jM,
-        this.$jD,
+        this.$hy,
+        this.$hM,
+        this.$hD,
         this.$H,
         this.$m,
         this.$s,
