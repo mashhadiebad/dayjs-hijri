12cf9fe7bcb77cd48d7e8c54176be21f88090810
M.MashhadiEbad <m.mashhadiebad@parto.com>
2025-01-23 13:14:36 +0330

    fix: Fix parse test

--- test/plugin.parse.test.js
+++ test/plugin.parse.test.js
@@ -1,40 +1,41 @@
-import dayjs from 'dayjs'
-import hijri from '../src'
+import dayjs from "dayjs";
+import hijri from "../src";
 
-dayjs.extend(hijri)
-dayjs.calendar('hijri')
+dayjs.extend(hijri);
 
-describe('Parse Valid String', () => {
-    // 2024-12-15
-    const date = dayjs('1446-06-14', { hijri: true })
+describe("Parse invalid String and null", () => {
+  test("Parse empty String", () => {
+    const date = dayjs("", { hijri: true });
+    expect(date.year()).toBeNaN();
+  });
+  test("Parse null", () => {
+    const date = dayjs(null, { hijri: true });
+    expect(date.year()).toBeNaN();
+  });
+});
+describe("Parse Valid String", () => {
+  // 2024-12-15
+  const date = dayjs("1446-06-14", { hijri: true }).calendar("hijri");
 
-    test('valid jalali date', () => {
-        expect(date.year()).toEqual(1446)
-        expect(date.month()).toEqual(5)
-        expect(date.date()).toEqual(14)
-    })
+  test("valid jalali date", () => {
+    expect(date.year()).toEqual(1446);
+    expect(date.month()).toEqual(5);
+    expect(date.date()).toEqual(14);
+  });
 
-    const date2 = dayjs('1446-06', { hijri: true })
+  const date2 = dayjs("1446-06", { hijri: true }).calendar("hijri");
 
-    test('valid jalali date without day', () => {
-        expect(date2.year()).toEqual(1446)
-        expect(date2.month()).toEqual(5)
-        expect(date2.date()).toEqual(1)
-    })
+  test("valid jalali date without day", () => {
+    expect(date2.year()).toEqual(1446);
+    expect(date2.month()).toEqual(5);
+    expect(date2.date()).toEqual(1);
+  });
 
-    const gregory = date.calendar('gregory')
+  const gregory = date.calendar("gregory");
 
-    test('convert to gregory', () => {
-        expect(gregory.year()).toEqual(2024)
-        expect(gregory.month()).toEqual(11)
-        expect(gregory.date()).toEqual(15)
-    })
-})
-
-it('Parse invalid String', () => {
-    const date = dayjs('', { hijri: true })
-    expect(date.year()).toBeNaN()
-
-    const date2 = dayjs(null, { hijri: true })
-    expect(date2.year()).toBeNaN()
-})
+  test("convert to gregory", () => {
+    expect(gregory.year()).toEqual(2024);
+    expect(gregory.month()).toEqual(11);
+    expect(gregory.date()).toEqual(15);
+  });
+});
