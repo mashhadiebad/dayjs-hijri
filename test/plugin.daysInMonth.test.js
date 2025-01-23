8c0ec5db3e43b5461f059499568efe087a59998d
M.MashhadiEbad <m.mashhadiebad@parto.com>
2025-01-23 11:48:20 +0330

    fix: Fix plugin.daysInMonth.test.js

--- test/plugin.daysInMonth.test.js
+++ test/plugin.daysInMonth.test.js
@@ -5,25 +5,25 @@ dayjs.extend(hijri);
 
 it("dayInMonth, months with 30 days", () => {
   expect(
-    dayjs("1446/07/13", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1466/01/20", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(30);
   expect(
-    dayjs("1446/09/01", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1446/04/30", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(30);
   expect(
-    dayjs("1446/11/13", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1430/10/02", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(30);
 });
 
 it("dayInMonth, months with 29 days", () => {
   expect(
-    dayjs("1446/08/13", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1466/02/11", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(29);
   expect(
-    dayjs("1446/10/01", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1443/10/06", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(29);
   expect(
-    dayjs("1446/12/03", { hijri: true }).calendar("hijri").daysInMonth(),
+    dayjs("1446/05/03", { hijri: true }).calendar("hijri").daysInMonth(),
   ).toEqual(29);
 });
 
